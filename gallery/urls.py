c1a9ddab21db4a345d38d17c340dac212fa83301
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-04 21:31:55 +0300

    create category model

---
+++ gallery/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    url('^$',views.gallery,name = 'gallery'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name='image')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
