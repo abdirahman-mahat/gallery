92a7c6af9f133b94383f129c7ed08ad5f48a5cd9
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-04 21:33:22 +0300

    add function for search

--- gallery/models.py
+++ gallery/models.py
@@ -14,3 +14,22 @@ class Category(models.Model):
         return self.name
 
 
+class Image(models.Model):
+    image_name = models.CharField(max_length = 60)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
+    photo = models.ImageField(upload_to = 'articles/')
+
+    def __str__(self):
+        return self.image_name
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        images = cls.objects.filter(category__icontains=search_term)
+        return images
