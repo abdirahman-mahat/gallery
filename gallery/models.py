6b22262b6dc2e93df713a79e2266e8026ed832e0
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-04 21:32:57 +0300

    create image model

--- gallery/models.py
+++ gallery/models.py
@@ -7,24 +7,10 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
-
-
-class Image(models.Model):
-    image_name = models.CharField(max_length = 60)
-    description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ManyToManyField(Category)
-    photo = models.ImageField(upload_to = 'articles/')
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
 
     def __str__(self):
-        return self.image_name
+        return self.name
 
-    @classmethod
-    def all_images(cls):
-        images = cls.objects.all()
-        return images
 
-    @classmethod
-    def search_by_category(cls,search_term):
-        images = cls.objects.filter(category__icontains=search_term)
-        return images
